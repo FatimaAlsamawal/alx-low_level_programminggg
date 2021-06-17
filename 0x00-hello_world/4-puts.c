5eceec66a37ceaaeb089e3eeee0a04cb991fc098
Michael Ndula <michaelndula@gmail.com>
2021-06-17 02:18:49 -0700

    a C program that prints an output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *Michael ndula is a C guru Ôo
+ *main - Intro to puts
+ *printing the output using puts function
+ *Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
