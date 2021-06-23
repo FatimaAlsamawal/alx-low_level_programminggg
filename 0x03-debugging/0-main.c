aa7d700d23781668a478a3d5302d375219c19744
Michael Ndula <michaelndula@gmail.com>
2021-06-23 06:12:54 -0700

    0. Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,16 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
-void positive_or_negative(int i)
+int main(void)
 {
+	int i;
+
 	i = 0;
 	positive_or_negative(i);
+
+	return (0);
 }
